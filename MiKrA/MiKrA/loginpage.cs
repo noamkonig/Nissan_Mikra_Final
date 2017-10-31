acbe4d4d42013ad628a2164c354d75f376722a49
noamhbk <12345678>
2017-10-31 15:33:02 +0200

    better code
    using page object, partly

---
+++ MiKrA/MiKrA/loginpage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+    public class loginpage
+    {
+        IWebDriver driver;
+        By username = By.Name("Username");
+        By passwordwrite = By.Name("Password");
+        By passwordclick = By.XPath("//*[@id=\"mockpass\"]/td/input");
+        By Loginclick = By.Name("login");
+
+        public loginpage (IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void typeusername ()
+        {
+            driver.FindElement(username).SendKeys("noamkonig@gmail.com");
+        }
+        public void typepassword ()
+        {
+            driver.FindElement(passwordclick).Click();
+            driver.FindElement(passwordwrite).SendKeys("noam4166");
+        }
+        public void clickonlogin ()
+        {
+            driver.FindElement(Loginclick).Click();
+        }
+    }
+
--- Program.cs
+++ Program.cs
@@ -10,25 +10,25 @@ using OpenQA.Selenium.Chrome;
 
 namespace yad2_jump
 {
+    
     class Program
     {
         static void Main(string[] args)
-        {
-            IWebElement emailfield;
-            IWebElement passfield;
+        {           
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Navigate().GoToUrl("http://www.yad2.co.il/");
-            emailfield = driver.FindElement(By.Name("Username"));
-            emailfield.SendKeys("noamkonig@gmail.com");
-            driver.FindElement(By.XPath("//*[@id=\"mockpass\"]/td/input")).Click();
-            driver.FindElement(By.Name("Password")).SendKeys("noam4166");
-            driver.FindElement(By.Name("login")).Click();
+            loginpage validation = new loginpage(driver);
+            validation.typeusername();
+            validation.typepassword();
+            validation.clickonlogin();
             driver.FindElement(By.XPath(" //img[contains(@src,'rechev.png')]")).Click();
-            driver.FindElement(By.XPath(" //*[@id='feed']/tbody/tr[2]/td[1]/div")).Click();
+            driver.FindElement(By.XPath(" //div[contains(text(),'השרון')]")).Click();
             driver.FindElement(By.XPath(" //*[@id='bounceRatingOrderBtn']/span")).Click();
-            Thread.Sleep(10000);
-            driver.Quit();
+            Thread.Sleep(2500);
+            
         }
+       
     }
+
 }
